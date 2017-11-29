227cae4efb54d2a6ec8b8e5c963a03d5620c5f80
Jeffrey Miller <millerjl1701@gmail.com>
2017-11-29 06:09:15 -0600

    init: added missing parameters to strings framework

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,6 +4,9 @@
 # Main class that includes all other classes for the ephemeris module.
 #
 # @param manage_python Whether or not to manage the installation of python, python-devel, pip, and virtualenv.
+# @param manage_python_dev Whether the python class should have python-devel package as absent or present.
+# @param manage_python_use_epel Whether or not the python class uses EPEL for installation of packages.
+# @param manage_python_virtualenv Whether the python class should have the virtualenv package as absent or present.
 # @param package_ensure Whether to install the ephemeris package, and/or what version. Values: 'present', 'latest', or a specific version number. Default value: present.
 # @param package_name Specifies the name of the package to install. Default value: 'ephemeris'.
 #
